7b6074ccfd5d43ba419f4147fb0dba7da6541e49
Cyrus Majd <cyrusmajd2001@gmail.com>
2020-01-09 00:33:26 -0500

    updated keylogger code

--- batch_scripts/training_data_collection_tools/keylogger.bat
+++ batch_scripts/training_data_collection_tools/keylogger.bat
@@ -9,5 +9,7 @@ set "chars= qwertyuiopasdfghjklzxcvbnm1234567890"
 :start
 cls
 choice /c %chars% >nul
->> %USERPROFILE%\OneDrive\Desktop\Gitz\razorReader\razor-reader\batch_scripts\test.txt echo !chars:~%errorlevel%,1!
+	REM The following line of code stores as a list in the file for all the things
+	REM >> %USERPROFILE%\OneDrive\Desktop\Gitz\razorReader\razor-reader\batch_scripts\training_data_collection_tools\keyLoggerTester.txt echo !chars:~%errorlevel%,1!
+> %USERPROFILE%\OneDrive\Desktop\Gitz\razorReader\razor-reader\batch_scripts\training_data_collection_tools\keyLoggerTester.txt echo !chars:~%errorlevel%,1!
 goto start
--- batch_scripts/training_data_collection_tools/lineCounter.bat
+++ batch_scripts/training_data_collection_tools/lineCounter.bat
@@ -1,7 +1,7 @@
 @echo off
 Set /A COUNTER=0
 setlocal ENABLEDELAYEDEXPANSION
-for /f "tokens=*" %%a in (tester.txt) do (
+for /f "tokens=*" %%a in (lineCounterTester.txt) do (
   echo line=%%a
   set /A COUNTER=COUNTER+1
   echo !COUNTER!
